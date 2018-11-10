ea01ae1c0d993da38792ce11433a74a41fb59949
Randle <kennyrandle09@gmail.com>
2018-11-10 17:47:22 +0100

    failsafe

--- InvestQ_for_my_investment_bank_corp/Form1.Designer.cs
+++ InvestQ_for_my_investment_bank_corp/Form1.Designer.cs
@@ -28,13 +28,349 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InvestAmountTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Month12RadioButton = new System.Windows.Forms.RadioButton();
+            this.Month6RadioButton = new System.Windows.Forms.RadioButton();
+            this.Month2RadioButton = new System.Windows.Forms.RadioButton();
+            this.Month1RadioButton = new System.Windows.Forms.RadioButton();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InvestAmountTextBox
+            // 
+            this.InvestAmountTextBox.Location = new System.Drawing.Point(331, 118);
+            this.InvestAmountTextBox.Name = "InvestAmountTextBox";
+            this.InvestAmountTextBox.Size = new System.Drawing.Size(167, 20);
+            this.InvestAmountTextBox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Month12RadioButton);
+            this.groupBox1.Controls.Add(this.Month6RadioButton);
+            this.groupBox1.Controls.Add(this.Month2RadioButton);
+            this.groupBox1.Controls.Add(this.Month1RadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(27, 219);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Investment terms";
+            // 
+            // Month12RadioButton
+            // 
+            this.Month12RadioButton.AutoSize = true;
+            this.Month12RadioButton.Location = new System.Drawing.Point(6, 145);
+            this.Month12RadioButton.Name = "Month12RadioButton";
+            this.Month12RadioButton.Size = new System.Drawing.Size(75, 17);
+            this.Month12RadioButton.TabIndex = 3;
+            this.Month12RadioButton.TabStop = true;
+            this.Month12RadioButton.Text = "12 Months";
+            this.Month12RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Month6RadioButton
+            // 
+            this.Month6RadioButton.AutoSize = true;
+            this.Month6RadioButton.Location = new System.Drawing.Point(6, 111);
+            this.Month6RadioButton.Name = "Month6RadioButton";
+            this.Month6RadioButton.Size = new System.Drawing.Size(69, 17);
+            this.Month6RadioButton.TabIndex = 2;
+            this.Month6RadioButton.TabStop = true;
+            this.Month6RadioButton.Text = "6 Months";
+            this.Month6RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Month2RadioButton
+            // 
+            this.Month2RadioButton.AutoSize = true;
+            this.Month2RadioButton.Location = new System.Drawing.Point(6, 70);
+            this.Month2RadioButton.Name = "Month2RadioButton";
+            this.Month2RadioButton.Size = new System.Drawing.Size(72, 17);
+            this.Month2RadioButton.TabIndex = 1;
+            this.Month2RadioButton.TabStop = true;
+            this.Month2RadioButton.Text = "3 Months ";
+            this.Month2RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Month1RadioButton
+            // 
+            this.Month1RadioButton.AutoSize = true;
+            this.Month1RadioButton.Location = new System.Drawing.Point(6, 31);
+            this.Month1RadioButton.Name = "Month1RadioButton";
+            this.Month1RadioButton.Size = new System.Drawing.Size(64, 17);
+            this.Month1RadioButton.TabIndex = 0;
+            this.Month1RadioButton.TabStop = true;
+            this.Month1RadioButton.Text = "1 Month";
+            this.Month1RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Location = new System.Drawing.Point(359, 144);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(94, 26);
+            this.DisplayButton.TabIndex = 2;
+            this.DisplayButton.Text = "Display";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.EmailTextBox);
+            this.groupBox2.Controls.Add(this.PhoneNumberTextBox);
+            this.groupBox2.Controls.Add(this.NameTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(253, 220);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 197);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tel Num:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(64, 95);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(181, 20);
+            this.EmailTextBox.TabIndex = 2;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(64, 56);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(181, 20);
+            this.PhoneNumberTextBox.TabIndex = 1;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(64, 19);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(181, 20);
+            this.NameTextBox.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 405);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Proceed ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Location = new System.Drawing.Point(566, 43);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(283, 95);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Search Transaction History ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Find my transactions ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(65, 32);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(163, 20);
+            this.textBox5.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(566, 230);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(283, 187);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Summary Details of all investment";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(608, 405);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Summary";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bodoni MT", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(281, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 33);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Invest Q";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(33, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Enter Investment Amount: ";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(345, 503);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(139, 30);
+            this.ExitButton.TabIndex = 14;
+            this.ExitButton.Text = "Exit ";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(21, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(242, 152);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Summary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(902, 659);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.InvestAmountTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Invest Q";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox InvestAmountTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Month12RadioButton;
+        private System.Windows.Forms.RadioButton Month6RadioButton;
+        private System.Windows.Forms.RadioButton Month2RadioButton;
+        private System.Windows.Forms.RadioButton Month1RadioButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- InvestQ_for_my_investment_bank_corp/Form1.cs
+++ InvestQ_for_my_investment_bank_corp/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace InvestQ_for_my_investment_bank_corp
 {
+    
     public partial class Form1 : Form
     {
+         decimal VALUE_OF_PRINCIPAL = 0m;
+
+        const decimal INTEREST_ON_AMOUNT_UPTO_100K_1MONTH = 0.5000m;
+        const decimal INTEREST_0N_AMOUNT_UPTO_100K_3MONTH = 0.6250m;
+        const decimal INTEREST_ON_AMOUNT_UPTO_100K_6MONTH = 0.7125m;
+        const decimal INTEREST_ON_AMOUNT_UPTO_100K_12MONTH= 1.1250m;
+
+        const decimal INTEREST_ON_AMOUNT_ABOVE_100K_1MONTH = 0.6000m;
+        const decimal INTEREST_ON_AMOUNT_ABOVE_100K_3MONTH = 0.7250m;
+        const decimal INTEREST_ON_AMOUNT_ABOVE_100K_6MONTH = 0.8125m;
+        const decimal INTEREST_ON_AMOUNT_ABOVE_100K_12MONTH= 1.2500m;
+
+         int TERM_1=1, TERM_3=3,  TERM_6=6, TERM_12 = 12;
+
+        decimal principal; double Amount; decimal rate; decimal term;
+       
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        { 
+            InvestAmountTextBox.MaxLength = 9;//sets the maximum lenght of input that can be accepted
+
+            if (InvestAmountTextBox.Text != "" &&  InvestAmountTextBox.TextLength==5 && InvestAmountTextBox.TextLength<6 )//check there aren't negatives, set max limit(max and min)
+            {
+                //principal = decimal.Parse(InvestAmountTextBox.Text);
+
+                // Amount = (principal * (Math.Pow ((double)(1 + INTEREST_ON_AMOUNT_UPTO_100K_1MONTH, TERM_1)) - 1).ToString;
+
+                decimal rawdata = decimal.Parse(InvestAmountTextBox.Text);
+                 CalculateInterest(rawdata, 6m, 6m);
+                label6.Text = Amount.ToString();
+                
+                
+
+
+
+            }
+            else if (InvestAmountTextBox.Text != "" && InvestAmountTextBox.TextLength==6 && InvestAmountTextBox.TextLength<7 )
+            {
+
+
+            }
+            else if (InvestAmountTextBox.Text !="" && InvestAmountTextBox.TextLength>7)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input", "Please enter a valid Number Not less than 5 digits", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } 
+        } 
+
+    
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void FileWriter()//method created to handle file writes
+        {
+            using (StreamWriter writer = new StreamWriter("ListOfInvestors.txt"))
+                writer.Write(Name + PhoneNumberTextBox + EmailTextBox);
+
+        }
+
+        public double CalculateInterest(decimal principal,  decimal rate, decimal term)
+        {
+            // principal((1 + (rate/100)/term )^term);
+            Amount =(double) principal * (Math.Pow(1 + (double)rate, (double)term) - 1);
+            return Amount;
+        }
+        public void FindMyTransactions()
+        {
+
         }
     }
 }
